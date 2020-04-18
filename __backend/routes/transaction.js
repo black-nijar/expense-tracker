9debefb87fba4a372172938293e396334f946cb7
Nijarudeen <nijarr2020@gmail.com>
2020-04-19 00:06:32 +0530

    GET: transaction

--- __backend/routes/transaction.js
+++ __backend/routes/transaction.js
@@ -2,11 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Transaction = require("../models/Transaction");
 
-// Create
+// POST Transaction
 router.post("/", async (req, res) => {
   try {
     const { text, amount } = req.body;
-    console.log('DETAILS :', text, amount)
     const newTransaction = new Transaction({
       text,
       amount,
@@ -19,8 +18,15 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/", (req, res) => {
-  res.send("History");
+// GET Transaction
+router.get("/", async (req, res) => {
+  try {
+    let transaction = await Transaction.find({});
+    res.json(transaction);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server error" });
+  }
 });
 
 router.delete("/:id", (req, res) => {
